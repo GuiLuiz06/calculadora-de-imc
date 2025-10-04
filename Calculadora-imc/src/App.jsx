570e84b31d697e7bf08dda88b87a132de4a4fdd5
Guilherme Luiz <margilepow1227@gmail.com>
2025-10-04 17:38:02 -0300

    first commit

---
+++ Calculadora-imc/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Calculadora from './components/calculadora'
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>Calculadora de imc</h1>
+      <Calculadora/>
+    </div>
+  )
+}
+
+export default App
---
+++ Calculadora-imc/src/components/Calculadora.jsx
@@ -0,0 +1,28 @@
+import './calculadora.css'
+
+const calculadora = () => {
+  return (
+    <div id="main-calc">
+        <form id='form-imc'>
+            <div className="form-inputs">
+                <div className="controle-form">
+                    <label htmlFor="altura">Altura:
+                        <input type="text" name='altura' id='altura' placeholder='exemplo 1,80' />
+                    </label>
+                </div>
+                <div className="controle-form">
+                    <label htmlFor="peso">Peso:
+                        <input type="text" name='peso' id='peso' placeholder='exemplo 74,3' />
+                    </label>
+                </div>
+            </div>
+            <div className="button-action">
+                <button>Calcular</button>
+                <button>Limpar</button>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default calculadora
